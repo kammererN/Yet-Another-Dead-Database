f3546f8d7d85d9e0f13ad57d6e86d822a698b156
nxrada <56482517+nxrada@users.noreply.github.com>
2022-11-29 22:55:00 -0500

    Completed integration between front and back ends.

--- flaskr/yadd.py
+++ flaskr/yadd.py
@@ -3,8 +3,24 @@ import sqlite3
 
 app = Flask(__name__)
 
-connection = sqlite3.connect("yadd.db")
-cursor = connection.cursor()
+
+# Helper functions to get tables from database.
+def get_albums():
+    conn = sqlite3.connect("yadd.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM albums")
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+def get_songs():
+    conn = sqlite3.connect("yadd.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM songs")
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
 
 @app.route("/")
@@ -29,8 +45,12 @@ def albums():
 
 @app.route("/yadd.html")
 def yadd():
-    return render_template("yadd.html")
+    std_albums = get_albums()
+    songs = get_songs()
+    print(std_albums, songs)
+    return render_template("yadd.html", albums=std_albums, songs=songs)
 
 
 if __name__ == '__main__':  # Runs on execution
     app.run()  # Creates site
+
